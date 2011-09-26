986b999dc675f824967be585a26fddefcea597b4
kolodyazhniy.kirill@gmail.com <kolodyazhniy.kirill@gmail.com@e2e0987e-fd59-510e-cb87-5912a4358cb7>
2011-09-26 14:57:12 +0000

    Initial import

---
+++ trunk/main.cpp
@@ -0,0 +1,55 @@
+#include "parallel.h"
+#include "timer.h"
+
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+typedef std::vector<double> ArrayType;
+
+ArrayType GetBigArray()
+{
+    const size_t n = 10000000;
+    ArrayType m(n);
+    for (size_t i = 0; i < n; ++i)
+    {
+        m[i] = i;
+    }
+    return m;
+}
+
+int main(int /*argc*/, char* /*argv*/[])
+{       
+    using namespace parallel;
+    Timer timer;
+
+    // serial test
+    ArrayType big_array = GetBigArray();
+    
+    timer.Start();
+    std::for_each(big_array.begin(), big_array.end(),
+        [](double& x)
+    {
+        x = std::sqrt(std::sqrt(x));
+    });
+    
+    std::cout << "Serial time is : " << timer.End() << " ms\n";
+
+    // parallel test
+    parallel::ThreadPool threadPool(4);
+    parallel::TaskManager manager(threadPool);
+
+    big_array = GetBigArray();
+
+    timer.Start();
+    parallel::parallel_for(big_array.begin(), big_array.end(),
+        [](double& x)
+    {
+        x = std::sqrt(std::sqrt(x));
+    }, manager);    
+    
+    std::cout << "Parallel time is : " << timer.End() << " ms\n";
+
+    return 0;
+}
---
+++ trunk/timer.h
@@ -0,0 +1,52 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include <windows.h>
+
+namespace parallel
+{
+class Timer
+{
+public:
+    Timer()
+    {
+         threadHandle = ::GetCurrentThread();
+        ::GetProcessAffinityMask(GetCurrentProcess(), &processAffinityMask, &systemMask);
+        ::SetThreadAffinityMask(threadHandle, 1);
+        ::QueryPerformanceFrequency(&frequency);
+        ::SetThreadAffinityMask(threadHandle, processAffinityMask);
+    }
+    void Start()
+    {
+        ::SetThreadAffinityMask(threadHandle, 1);
+        ::QueryPerformanceCounter(&startTime);
+        ::SetThreadAffinityMask(threadHandle, processAffinityMask);
+    }
+    double End()
+    {
+        ::SetThreadAffinityMask(threadHandle, 1);
+        ::QueryPerformanceCounter(&endTime);
+        ::SetThreadAffinityMask(threadHandle, processAffinityMask);
+
+         __int64 elapsedTime = endTime.QuadPart - startTime.QuadPart;
+        double const mseconds = double(elapsedTime) / (double(frequency.QuadPart) / 1000.0);
+        return mseconds;
+    }
+private:
+    Timer(const Timer&);
+    const Timer& operator=(const Timer&);
+private:
+    LARGE_INTEGER startTime;
+    LARGE_INTEGER endTime;
+	LARGE_INTEGER frequency;
+    void* threadHandle;
+#if defined(_WIN64)
+    unsigned __int64 processAffinityMask;
+#else
+    unsigned long processAffinityMask;
+#endif
+    DWORD_PTR systemMask;
+};
+}
+
+#endif
